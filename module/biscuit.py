4f5cee7d71ba6851dafd3992c3382ababc73f559
sequierh <hugo.sequier@etu.umontpellier.fr>
2023-11-30 01:18:29 +0100

    New Main, add comments on Implementation (to just add it on PDF)

--- module/biscuit.py
+++ module/biscuit.py
@@ -1,5 +1,20 @@
 class Biscuit:
+    """
+    This class is all about individual biscuits - their type, size, value, 
+    and how finicky they are about defects. Each biscuit is an opportunity to earn some dough, 
+    but we've got to play by the rules.
+    """
     def __init__(self, type, length, value, max_defects) :
+        """
+        We need to define what it is, how big it is, how valuable it is, 
+        and what defects it can tolerate.
+
+        Parameters:
+        type (str): The type or variety of the biscuit.
+        length (int): How long the biscuit is, because size matters here!
+        value (int): What's the biscuit worth? Higher value means more profit.
+        max_defects (dict): A playbook of what defects this biscuit can handle and how many.
+        """
         self.type = type 
         self.length = length
         self.value = value
@@ -11,11 +26,41 @@ class Biscuit:
         }
     
     def update_defects(self, dict_defect, type_defect, nb_defect=1):
+        """
+        Biscuits are picky about their neighbors. This method updates the defect count to see if 
+        our biscuit can handle the local defect environment. It's like checking the air quality 
+        before going for a run.
+
+        Parameters:
+        dict_defect (dict): Current defect count around the biscuit.
+        type_defect (str): The type of defect we're particularly looking at.
+        nb_defect (int, optional): How many defects of this type are there? Defaults to 1.
+
+        Returns:
+        dict: Updated defect count, now including the new defects we just counted.
+        """
         for key, value in dict_defect.items() :
             if key == type_defect :
                 dict_defect[key] = value + nb_defect
         return dict_defect
 
-    def check_defect(self, defects_on_roll):
-        return all(defects_on_roll[k] <= self.max_defects[k] for k in defects_on_roll if k in self.max_defects)
-        
+    def check_defect(self, type_defect, nb_defect=1):
+        """
+        Can our biscuit handle the defects at its planned position on the roll? 
+        This function is like a quality control inspector. It checks if the local defect levels 
+        exceed our biscuit's tolerance. If it's too much, we might have to find a new spot.
+
+        Parameters:
+        type_defect (str): The type of defect we're evaluating.
+        nb_defect (int): The number of defects of this type.
+
+        Returns:
+        bool: True if the biscuit can deal with these defects, False if it's a no-go.
+        """
+        copy_defect = self.defects.copy()
+        copy_defect = self.update_defects(copy_defect, type_defect, nb_defect)
+        for key, value in copy_defect.items() :
+            if value > self.max_defects[key]:
+                print("Can't add biscuit")
+                return False
+        return True
--- module/roll.py
+++ module/roll.py
@@ -1,9 +1,35 @@
 class Roll:
+    """
+    The Roll class represents a roll of dough in the biscuit manufacturing process. 
+    It's designed to keep track of the dough's length and the distribution of defects across it. 
+    Think of it as a map of where we can (or can't) place biscuits based on these defects.
+    """
+
     def __init__(self, defects, length=500):
+        """
+        When we create a new roll, we need to know about any defects it has and its length.
+        This setup is crucial for figuring out how to optimally place the biscuits later on.
+
+        Parameters:
+        defects (DataFrame): Information about where defects are on the dough and their types.
+        length (int, optional): How long the dough roll is. Defaults to 500 units.
+        """
         self.length = length
         self.roll = self.build_roll(defects, length)
 
     def build_roll(self, defects, length):
+        """
+        Here we construct our roll of dough. It's a bit like setting up the game board.
+        We take the length of the roll and the defects data to create a detailed map of the roll.
+        Each section of the roll will have information about whether it's usable or has defects.
+
+        Parameters:
+        defects (DataFrame): Detailed data on the types and locations of defects in the dough.
+        length (int): The total length of the dough roll we're working with.
+
+        Returns:
+        list: A structured representation of the dough roll, section by section, including defects.
+        """
         roll = [{"empty" : -1, 'defects' : {}} for _ in range(0, length)]
         for idx in range(0,length):
             mask_position = defects['x'] == idx
@@ -14,24 +40,51 @@ class Roll:
         return roll
 
     def check_position_empty(self, position, biscuit_length=1):
+        """
+        Before placing a biscuit, we need to make sure the spot is empty and not already taken.
+        This function checks if a specific section of the roll is free of other biscuits.
+
+        Parameters:
+        position (int): The starting position on the roll where we want to place the biscuit.
+        biscuit_length (int, optional): How long the biscuit is. Defaults to 1 unit.
+
+        Returns:
+        bool: True if the space is empty and ready for a new biscuit, False otherwise.
+        """
         if position >=0 and (position + biscuit_length) < self.length :
             return all(element["empty"] == -1 for element in self.roll[position : (position + biscuit_length)])
         print("Can't add biscuit")
         return False
     
     def add_biscuit(self, position, biscuit):
+        """
+        Time to place a biscuit.
+        This function not only checks if the spot is available but also makes sure that the biscuit
+        meets our quality standards by checking against the defect constraints.
+
+        Parameters:
+        position (int): Where on the roll we're trying to place the biscuit.
+        biscuit (Biscuit): The biscuit object we want to add to our roll.
+
+        Returns:
+        list: An updated view of our roll, now with the new biscuit added (if everything went well).
+        """
         if self.check_position_empty(position, biscuit.length):
-            defects_at_position = [x["defects"] for x in self.roll[position : position + biscuit.length]]
-            sum_defects_at_position = {}
-            for d in defects_at_position:
-                for key, value in d.items():
-                    sum_defects_at_position[key] = sum_defects_at_position.get(key, 0) + value
-            if biscuit.check_defect(sum_defects_at_position):
+            defects_at_position = self.roll[position]["defects"]
+            if all(biscuit.check_defect(defect_type, nb_defect) for defect_type,nb_defect in defects_at_position.items()):
                 for idx_length in range(0, biscuit.length):
                     self.roll[position + idx_length]["empty"] = biscuit
         return self.roll
     
     def solution(self):
+        """
+        After all our hard work placing biscuits, let's see what we've got!
+        This function tallies up the total value of all the biscuits on the roll.
+        It's like calculating the score of how well we did in optimizing our biscuit placement.
+
+        Returns:
+        int: The total value of all the biscuits on the roll.
+        """
         cpt_value = 0
         idx = 0
         while idx < self.length:
